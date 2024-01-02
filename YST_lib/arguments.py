a3f629afb5264a93ed57f60353655b87707d427c
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2024-01-03 00:23:07 +0100

    Updated Location save logs to /txt
    Added new features
    Optimized codes

--- YST_lib/arguments.py
+++ YST_lib/arguments.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 from YST_lib.required import *
 
 def check_arg():
@@ -14,22 +15,7 @@ def check_arg():
         return args
 
 arguments = check_arg()
-
-"""
-
-Traceback (most recent call last):
-  File "/home/programmer/OBS Data/Tools/YST2/YST.py", line 4, in <module>
-    main()
-  File "/home/programmer/OBS Data/Tools/YST2/YST_lib/main.py", line 81, in main
-    request_video()
-  File "/home/programmer/OBS Data/Tools/YST2/YST_lib/main.py", line 46, in request_video
-    query2 = x = f'https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id={video_id}&key={API_KEY}'
-                                                                                        ^^^^^^^^
-NameError: cannot access free variable 'video_id' where it is not associated with a value in enclosing scope
-
-// python3 YST.py -channel_id=UCifZaTQPiHE2QRgEwDNfhug -video_id=wdHGVUiFGws
-
-"""
+arguments2 = copy.copy(arguments)
 
 if arguments:
     for arg_name, arg_value in arguments.items():
@@ -40,83 +26,35 @@ if arguments:
             elif arg_name == "video_id" and not used_videoID:
                 video_id = arg_value
                 used_videoID = True
+            elif arg_name == "latest_video" and not used_latestVideo:
+                latest_video = arg_value
+                used_latestVideo = True
         except:
             pass
 
     if not used_channelID:
         channel_query = input(f"{bcolors.BOLD}Paste Your Channel ID or Youtube Link > ")
         if len(channel_query) > 1:
-            channel_id = channel_query.split("channel/")[1]
+            if "youtube.com/channel/" not in channel_query:
+                arguments2['channel_id'] = channel_query
+            else:
+                arguments2['channel_id'] = channel_query.split("channel/")[1]
             used_channelID = True
         else:
             exit(f"{bcolors.FAIL}No Found Channel ID or Youtube Link{bcolors.DEFAULT}")
 
+
+    if 'latest_video' in arguments:
+        used_videoID = True
+
     if not used_videoID:
         video_query = input(f"{bcolors.BOLD}Paste Your Video ID or Youtube Link > ")
         if len(video_query) > 1:
-            video_id = video_query.split("?v=")[1]
+            if "?v=" not in video_query:
+                arguments2['video_id'] = video_query
+            else:
+                arguments2['video_id'] = video_query.split("?v=")[1]
             used_videoID = True
         else:
             exit(f"{bcolors.FAIL}No Found Video ID or Youtube Link{bcolors.DEFAULT}")
-# d = check_arg(1, "-channel_id")
-# if (d == None):
-#     try:
-#       channel_query = input(f"{bcolors.BOLD}Paste Your Channel ID or Youtube Link > ")
-#       if len(channel_query) > 1:
-#         channel_id = channel_query.split("channel/")[1]
-#         pass
-#       else:
-#         exit(f"{bcolors.FAIL}No Found Channel ID or Youtube Link{bcolors.DEFAULT}")
-#     except:
-#       channel_id = channel_query
-# else:
-#     channel_id = d
-#     # channel_id = UC9QeWFl9wDaC3NH25v8VpgQ
-# d = check_arg(2, "-video_id")
-# if (d == None):
-#     try:
-#       video_query = input(f"{bcolors.BOLD}Paste Your Video ID or Youtube Link > ")
-#       if len(video_query) > 1:
-#         video_id = video_query.split("v=")[1]
-#         pass
-#       else:
-#         exit(f"{bcolors.FAIL}No Found Channel ID or Youtube Link{bcolors.DEFAULT}")      
-#       pass
-#     except:
-#       video_id = video_query
-# else:
-#     video_id = d
-#     # video_id = OyWhdZZkEtY
-
-# d = check_arg(3, "-sleep_time")
-# if (d == None):
-#     pass
-# else:
-#     try:
-#       sleep_time = int(d)
-#     except ValueError:
-#       exit(f"{bcolors.FAIL}Invalid Number{bcolors.DEFAULT}")
-#     except Exception:
-#       exit(f"{bcolors.FAIL}Other error{bcolors.DEFAULT}") 
-
-# d = check_arg(4, "-log_mode")
-# if (d == None):
-#     pass
-# else:
-#     try:
-#       logmode = bool(d)
-#     except ValueError:
-#       exit(f"{bcolors.FAIL}Invalid value True/False{bcolors.DEFAULT}")
-#     except Exception:
-#       exit(f"{bcolors.FAIL}Other error{bcolors.DEFAULT}")
-
-# d = check_arg(5, "-get_latest_video")
-# if (d == None):
-#     pass
-# else:
-#     try:
-#       get_latest_video = bool(d)
-#     except ValueError:
-#       exit(f"{bcolors.FAIL}Invalid value True/False{bcolors.DEFAULT}")
-#     except Exception:
-#       exit(f"{bcolors.FAIL}Other error{bcolors.DEFAULT}")
+       
--- YST_lib/main.py
+++ YST_lib/main.py
@@ -1,6 +1,7 @@
 from time import localtime, sleep, strftime
 from YST_lib.required import *
 from YST_lib.arguments import *
+
 def main():
     print(sep)
     print("	YouTube Stats Tool (v 1.0 by https://github.com/klubuntu)")
@@ -11,13 +12,13 @@ def main():
         current_time = strftime("(%d-%m-%Y) - %H:%M:%S", t)
         print(f"{bcolors.REMBOLD}{current_time}")
 
-    def get_latest_eventid():
+    def get_latest_eventid(channel_id):
         query = f"https://www.googleapis.com/youtube/v3/search?part=snippet&channelId={channel_id}&maxResults=1&type=video&order=date&key={API_KEY}"
         response = requests.get(query)
         data = json.loads(response.text)
         return data["items"][0]["id"]["videoId"]
 
-    def request_channel():
+    def request_channel(channel_id):
         global subs, videoCount, viewCount
         query = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={channel_id}&key={API_KEY}"
         response = requests.get(query)
@@ -37,11 +38,10 @@ def main():
             f.close()
         except KeyError as e:
             print(e)
-            print(query)
             print(f"{bcolors.FAIL}Invalid URL or Channel ID{bcolors.DEFAULT}")
             exit(sep)
 
-    def request_video():
+    def request_video(video_id):
         global views,likes,comments
         query2 = x = f'https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id={video_id}&key={API_KEY}'
         response2 = requests.get(query2)
@@ -63,7 +63,8 @@ def main():
             # print(e)
             print(f"{bcolors.FAIL}Invalid URL or Video ID{bcolors.DEFAULT}")
             exit(sep)
-
+    def test():
+        print(1)
     def result():
         print(f"{bcolors.DEFAULT}{bcolors.LIGHTGREEN}Subscribers: {subs}")
         print(f"{bcolors.OKCYAN}Channel Views: {viewCount}")
@@ -73,12 +74,23 @@ def main():
         print(f"{bcolors.WARNING}Video Views: {views}{bcolors.DEFAULT}")
         print("")
     try:
-        if not (logmode):
+        if (arguments2.get('channel_id')):
+             channel_id = arguments2.get('channel_id')
+        if (arguments2.get('video_id')):
+             video_id = arguments2.get('video_id')
+        #video_id = arguments.get('video_id') if 'video_id' in arguments and arguments['video_id'] is not None else ""
+        log_mode = arguments['log_mode'] if arguments['log_mode'] is not None else False
+        latest_video = arguments.get('latest_video') if 'latest_video' in arguments and arguments['latest_video'] is not None else False
+        if (latest_video):
+           video_id = get_latest_eventid(channel_id)
+                
+            
+        if not (log_mode):
             progress = "-"
             print(f"Start Logging to Folder {soft_dir}")
             while(True):
-                request_channel()
-                request_video()
+                request_channel(channel_id)
+                request_video(video_id)
                 print(f'{bcolors.WARNING}{progress}', end='\r')
                 progress = progress + "-"
                 if (progress == "------------------------------------------------------------"):
@@ -87,14 +99,12 @@ def main():
         else:
             while(True):
                 date()
-                print(get_latest_eventid())
-                if(get_latest_video):
-                    video_id = get_latest_eventid()
-                    print(video_id)
-                request_channel()
-                request_video()
+                request_channel(channel_id)
+                request_video(video_id)
                 result()
                 sleep(sleep_time)
+                
+          
 
     except KeyboardInterrupt:
         print(
@@ -104,7 +114,7 @@ def main():
 if __name__ == '__main__':
 
     try:
-        get_eventid()
+        get_eventid(channel_id)
         # main()
         
     except KeyboardInterrupt:
